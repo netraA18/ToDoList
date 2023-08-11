d9807a4848e1263b9ba4877152a85147e91bd018
netraA18 <netraamin13@gmail.com>
2023-08-11 01:20:44 -0400

    revised

--- Main.java
+++ Main.java
@@ -1,55 +1,103 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
+
 
 
 public class Main {
+    public static int numbOfTask; //number of tasks the user wants to add
 
     public static void homePage() {
-            System.out.println("--ToDo List--\n");
-            System.out.println("1. Add Task");
-            System.out.println("2. View All Tasks");
-            System.out.println("3. Edit Task\n");
+        System.out.println("--ToDo List--\n");
+        System.out.println("1. Add Task");
+        System.out.println("2. View All Tasks");
+        System.out.println("3. Edit Task\n");
 
         }
+
+        public static void addTask(Scanner scanner, List<Task> allTasks) {
+        System.out.println();
+        System.out.print("How many tasks? ");
+        String taskNumb = scanner.nextLine();
+        numbOfTask = Integer.parseInt(taskNumb);
+    
+        System.out.println();
+    
+        for (int i = 1; i <= numbOfTask; i++) {
+            System.out.print("Enter task " + i + " name: ");
+            String taskName = scanner.nextLine(); //sets the input to the variable taskName
+    
+            System.out.print("Enter task " + i + " description: ");
+            String taskDescription = scanner.nextLine();
+    
+            System.out.print("Enter task " + i + " due date: ");
+            String taskDueDate = scanner.nextLine();
+    
+            Task newTask = new Task(taskName, taskDescription, taskDueDate); 
+            allTasks.add(newTask);
+    
+            System.out.println();
+        }
+    }
+
+    
+    public static void viewAllTasks(List<Task> allTasks) {
+        int taskCount = 1;
+
+        for (Task task : allTasks) { //for all task in the class Task, iterate through the arrayList
+            System.out.println("Task " + taskCount + ": ");
+            System.out.println("Name: " + task.getTaskName());
+            System.out.println("Description: " + task.getTaskDescription());
+            System.out.println("Due Date: " + task.getTaskDueDate());
+            System.out.println();
+            taskCount++;
+        }
+    }
+    
     
     public static void main(String[] args) {
-        Task task = new Task();
-        // task.displayTask();
+        List<Task> allTasks = new ArrayList<>();
+
         homePage();
-        
-        
-        
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        String numb = scanner.nextLine();
-        
-        int numbChoice = Integer.parseInt(numb);
+        int numbChoice = Integer.parseInt(scanner.nextLine());
 
-        if (numbChoice == 1) {
-            task.addTask();
-        }
+        Task taskToEdit = null; //temporarily store a reference to a task
 
-        
         while (numbChoice != 0) {
-            numbChoice = task.ask_Numb();
+            switch (numbChoice) {
+                case 1:
+                    addTask(scanner, allTasks);
+                    break;
+                case 2:
+                    viewAllTasks(allTasks);
+                    break;
+                case 3:
+                    taskToEdit.editTask(scanner); 
+                    break;
+            }
 
             
-            if (numbChoice == 2) {
-                task.viewallTasks();
-            }
 
-            if (numbChoice == 0) {
-                homePage();
-            }
-
-            if (numbChoice == 1) {
-            task.addTask();
-        }
+            System.out.print("Enter a number: ");
+            numbChoice = Integer.parseInt(scanner.nextLine());
 
             if (numbChoice == 3) {
-                task.editTasks();
+                System.out.print("Enter the task number to edit: ");
+                int taskNumberToEdit = Integer.parseInt(scanner.nextLine());
+
+                if (taskNumberToEdit >= 1 && taskNumberToEdit <= allTasks.size()) {
+                    taskToEdit = allTasks.get(taskNumberToEdit - 1); //to get the correct task 
+                } else {
+                    System.out.println("Invalid task number.");
+                    taskToEdit = null; 
+                }
             }
         }
+
         scanner.close();
-        
     }
+
 }
--- Task.java
+++ Task.java
@@ -1,121 +1,61 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Task {
-    public String taskName;
-    public String taskDescription;
-    public String taskDueDate;
-    public String taskStatus;
-    public int numbOfTask;
-    ArrayList<String> allTasks = new ArrayList<String>();
-    public int numbChoice; 
-    Scanner scanner = new Scanner(System.in);
+    private String taskDescription;
+    private String taskName;
+    private String taskDueDate;
 
-    public void addTask() {
-
-        System.out.println();
-        System.out.print("How many tasks? ");
-        String taskNumb = scanner.nextLine();
-        numbOfTask = Integer.parseInt(taskNumb);
-        int i;
-        System.out.println();
-
-        for (i = 1; i <= numbOfTask; i++) {
-            System.out.print("Enter task " + i + " name: ");
-            this.taskName = scanner.nextLine();
-            allTasks.add(taskName);
-
-            System.out.print("Enter task " + i + " description: ");
-            this.taskDescription = scanner.nextLine();
-            allTasks.add(taskDescription);
-
-            System.out.print("Enter task " + i + " due date: ");
-            this.taskDueDate = scanner.nextLine();
-            allTasks.add(taskDueDate);
-
-            System.out.println();
-        }
-
+    public Task(String taskName, String taskDescription, String taskDueDate) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+        this.taskDueDate = taskDueDate;
     }
 
-    public int ask_Numb() {
-        System.out.print("Enter a number or press 0 to go to home page: ");
-        String numb = scanner.nextLine();
-        System.out.println();
-        numbChoice = Integer.parseInt(numb);
-        // scanner.close();
-
-        return numbChoice;
+    public String getTaskName() {
+        return taskName;
     }
 
-    public void viewallTasks() {
-        int n = allTasks.size();
-
-        int taskCount = 1;
-        for (int j = 0; j < n; j++) {
-            if (n == 3) {
-                System.out.println(allTasks);
-                break;
-            } else if (n > 3) {
-                List<String> selectedList = allTasks.subList(j, j + 3);
-                j += 2;
-                System.out.println("Task " + taskCount + ": \n" + selectedList);
-                System.out.println();
-                taskCount++;
-            }
-        }
-
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
     }
 
-    public void changeTasks() {
-        int n = allTasks.size();
-
-        int taskCount = 1;
-        for (int j = 0; j < n; j += 3) {
-            List<String> selectedList = allTasks.subList(j, Math.min(j + 3, n));
-            System.out.println("Task " + taskCount + ": \n" + selectedList);
-
-            
-            System.out.print("Do you want to edit this task? ");
-            String taskNumb = scanner.nextLine();
-            System.out.println();
-            if (taskNumb.equals("yes")) {
-                editSingleTask(selectedList);
-            }
-
-            System.out.println();
-            taskCount++;
-        }
+    public String getTaskDescription() {
+        return taskDescription;
     }
 
-    private void editSingleTask(List<String> selectedList) { //private limits it to the task class 
-        
-        System.out.print("a) Name, b) description, or c) due date? enter letter: ");
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public String getTaskDueDate() {
+        return taskDueDate;
+    }
+
+    public void setTaskDueDate(String taskDueDate) {
+        this.taskDueDate = taskDueDate;
+    }
+
+    public void editTask(Scanner scanner) {
+        System.out.print("a) Name, b) Description, c) Due date? Enter letter: ");
         String editLetter = scanner.nextLine();
         System.out.println();
 
-        switch (editLetter) { //pass in editLetter as the parameter -- compares it to this
+        switch (editLetter.toLowerCase()) {
             case "a":
-                System.out.print("Enter name: ");
+                System.out.print("Enter new name: ");
                 this.taskName = scanner.nextLine();
-                selectedList.set(0, this.taskName);
-                break; 
+                break;
             case "b":
-                System.out.print("Enter description: ");
+                System.out.print("Enter new description: ");
                 this.taskDescription = scanner.nextLine();
-                selectedList.set(1, this.taskDescription);
                 break;
             case "c":
-                System.out.print("Enter due date: ");
+                System.out.print("Enter new due date: ");
                 this.taskDueDate = scanner.nextLine();
-                selectedList.set(2, this.taskDueDate);
                 break;
-            
+            default:
+                System.out.println("Invalid choice.");
+                break;
         }
-    }
-
-    public void editTasks() {
-        changeTasks();
     }
 }
